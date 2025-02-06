2079e75f4125c19c87bc1bcff62f189c5ce95370
Marlo <marsunlop@gmail.com>
2025-02-06 13:08:02 +0100

    fixed bad return at evaluation

--- pyae/evaluation.py
+++ pyae/evaluation.py
@@ -268,11 +268,11 @@ def compute_loss(training_manager, dataloader, loss_func, on_vae):
     """
     losses = []
     for batch in dataloader:
-        loss = training_manager._compute_forward_loss(batch, return_outputs=True, loss_func=loss_func)
+        loss = training_manager._compute_forward_loss(batch, loss_func=loss_func)
 
-        losses.append(loss)
+        losses.append(loss.item())
     
-    return torch.cat(losses)
+    return torch.tensor(losses)
 
 def get_loss_statistics(training_manager, dataloader, loss_func, on_vae=False):
     """
